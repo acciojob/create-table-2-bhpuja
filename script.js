4b48a3c06ef28c07c15092c8dba52aa531894387
bhpuja <vpuja0421@gmail.com>
2025-01-04 16:13:23 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,24 @@
+function createTable() {
+    // Get the number of rows and columns from the user
+    let rn = prompt("Input number of rows");
+    let cn = prompt("Input number of columns");
+
+    // Get the reference of the table
+    let table = document.getElementById("myTable");
+
+    // Create rows and columns
+    for(let i=0; i<rn; i++) {
+        let row = document.createElement("tr");
+
+        for(let j=0; j<cn; j++) {
+            let cell = document.createElement("td");
+            // Add text to the cell
+            cell.textContent = Row-${i} Column-${j};
+            // Append the cell to the row
+            row.appendChild(cell);
+        }
+
+        // Append the row to the table
+        table.appendChild(row);
+    }
+}
